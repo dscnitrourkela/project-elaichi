bd8bc6cab63019e6cd3d461dff8fa61fa71c0870
Thesmader <dsmarak17@gmail.com>
2020-06-17 01:59:03 +0530

    Restructure project

---
+++ elaichi/lib/app/router.gr.dart
@@ -0,0 +1,47 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// AutoRouteGenerator
+// **************************************************************************
+
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:auto_route/auto_route.dart';
+import 'package:elaichi/ui/views/startup/startup_view.dart';
+import 'package:elaichi/ui/views/home/home_view.dart';
+
+abstract class Routes {
+  static const startupViewRoute = '/';
+  static const homeViewRoute = '/home-view-route';
+  static const all = {
+    startupViewRoute,
+    homeViewRoute,
+  };
+}
+
+class Router extends RouterBase {
+  @override
+  Set<String> get allRoutes => Routes.all;
+
+  @Deprecated('call ExtendedNavigator.ofRouter<Router>() directly')
+  static ExtendedNavigatorState get navigator =>
+      ExtendedNavigator.ofRouter<Router>();
+
+  @override
+  Route<dynamic> onGenerateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.startupViewRoute:
+        return MaterialPageRoute<dynamic>(
+          builder: (context) => StartupView(),
+          settings: settings,
+        );
+      case Routes.homeViewRoute:
+        return MaterialPageRoute<dynamic>(
+          builder: (context) => HomeView(),
+          settings: settings,
+        );
+      default:
+        return unknownRoutePage(settings.name);
+    }
+  }
+}
--- elaichi/test/widget_test.dart
+++ elaichi/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:elaichi/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(NITRCentralApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
