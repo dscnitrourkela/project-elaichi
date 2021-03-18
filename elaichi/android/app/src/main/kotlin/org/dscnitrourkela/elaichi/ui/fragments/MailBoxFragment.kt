dba81c16e206020f7a48a08ec2ac0e00787487e6
sachin2dehury <sachindehury2015@gmail.com>
2021-03-18 08:33:47 +0530

    Added Owl mail flutter Support
    - Added getMails and Login in MainActivity
    - Updated dependency

--- elaichi/android/app/src/main/kotlin/org/dscnitrourkela/elaichi/ui/fragments/MailBoxFragment.kt
+++ elaichi/android/app/src/main/kotlin/org/dscnitrourkela/elaichi/ui/fragments/MailBoxFragment.kt
@@ -9,32 +9,33 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import androidx.paging.ExperimentalPagingApi
 import androidx.paging.LoadState
 import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.InternalCoroutinesApi
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
 import org.dscnitrourkela.elaichi.NavGraphDirections
 import org.dscnitrourkela.elaichi.R
 import org.dscnitrourkela.elaichi.adapters.MailBoxAdapter
 import org.dscnitrourkela.elaichi.databinding.FragmentMailBoxBinding
 import org.dscnitrourkela.elaichi.others.ApiConstants
 import org.dscnitrourkela.elaichi.ui.viewmodels.MailBoxViewModel
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import org.dscnitrourkela.elaichi.utils.showSnackbar
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class MailBoxFragment(private val request: String) : Fragment(R.layout.fragment_mail_box) {
+open class MailBoxFragment : Fragment(R.layout.fragment_mail_box) {
 
     private var _binding: FragmentMailBoxBinding? = null
     private val binding: FragmentMailBoxBinding get() = _binding!!
 
     private val viewModel: MailBoxViewModel by activityViewModels()
 
-//    private val args: MailBoxFragmentArgs by navArgs()
+    private val args: MailBoxFragmentArgs by navArgs()
 
     private var job: Job? = null
 
@@ -70,12 +71,6 @@ class MailBoxFragment(private val request: String) : Fragment(R.layout.fragment_
         }
     }
 
-    @ExperimentalPagingApi
-    override fun onResume() {
-        super.onResume()
-        getJob()
-    }
-
     private fun setupAdapter() = mailBoxAdapter.setupOnItemClickListener {
         findNavController().navigate(
             NavGraphDirections.actionToMailItemsFragment(it.conversationId, it.id)
@@ -88,7 +83,7 @@ class MailBoxFragment(private val request: String) : Fragment(R.layout.fragment_
     }
 
     @ExperimentalPagingApi
-    private fun getJob(query: String = request) {
+    private fun getJob(query: String = args.request) {
         job?.cancel()
         job = lifecycleScope.launch {
             viewModel.getMails(query).collectLatest {
@@ -118,7 +113,6 @@ class MailBoxFragment(private val request: String) : Fragment(R.layout.fragment_
 
     @ExperimentalPagingApi
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        menu.removeItem(R.id.searchBar)
         inflater.inflate(R.menu.search_menu, menu)
         val searchView = menu.findItem(R.id.searchBar).actionView as SearchView
         searchView.queryHint = getString(R.string.search)
