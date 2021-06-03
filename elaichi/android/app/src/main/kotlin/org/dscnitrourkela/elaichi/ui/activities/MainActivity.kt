3b2c82c192f3df49b69b38fd2e15a45e10bf99f1
Chinmay <chinmay.kabi@live.com>
2021-06-04 01:05:08 +0530

    webmail integration is broken

--- elaichi/android/app/src/main/kotlin/org/dscnitrourkela/elaichi/ui/activities/MainActivity.kt
+++ elaichi/android/app/src/main/kotlin/org/dscnitrourkela/elaichi/ui/activities/MainActivity.kt
@@ -1,21 +1,18 @@
 package org.dscnitrourkela.elaichi.ui.activities
 
-import android.content.Intent
-import android.os.Bundle
 import androidx.annotation.NonNull
-import androidx.lifecycle.*
-import androidx.paging.ExperimentalPagingApi
 import dagger.hilt.android.AndroidEntryPoint
-import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import okhttp3.Credentials
 import org.dscnitrourkela.elaichi.R
+import org.dscnitrourkela.elaichi.api.data.Mail
 import org.dscnitrourkela.elaichi.repository.MailRepository
 import javax.inject.Inject
 
@@ -28,22 +25,44 @@ class MainActivity : FlutterFragmentActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine)
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
-            .setMethodCallHandler { call, _ ->
+            .setMethodCallHandler { call, result ->
                 if (call.method.equals("startOwlMail", ignoreCase = true)) {
-                    startActivity(Intent(this, MailActivity::class.java))
+                    ensureLogin()
+                }
+                if (call.method.equals("getMails", ignoreCase = true)) {
+                    result.success(getMailsList().size)
                 }
             }
     }
 
-    suspend fun getMails()=
-        mailRepository.getMails(getString(R.string.inbox),0)
+    fun getMailsList():List<Mail>{
+        var list:List<Mail> = emptyList()
+        CoroutineScope(Dispatchers.IO).launch {
+            list = getMails()
+            print("getting list inside coroutine done")
 
+        }
+        return list
+    }
+
+    fun ensureLogin():Unit{
+        CoroutineScope(Dispatchers.IO).launch {
+            login()
+            print("login inside coroutine done")
+
+        }
+    }
+    suspend fun getMails()=
+        mailRepository.getMails(getString(R.string.inbox),0).first()
+
+    //TODO Change the hardcoded email password
     suspend fun login() {
-        mailRepository.setCredential(Credentials.basic("RollNO","Password"))
+        mailRepository.setCredential(Credentials.basic("Roll no","Password."))
         mailRepository.login()
+        print("Ho gaya login, aage badho")
     }
 
     companion object {
-        private const val CHANNEL = "OWL_MAIL"
+        private const val CHANNEL = "org.dscnitrourkela.elaichi"
     }
 }
