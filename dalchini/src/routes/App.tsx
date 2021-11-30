6a9b608487b5c2a17f98f81d88db07e349f279d6
Ritesh Patil <riteshsp2000@gmail.com>
2021-11-30 17:08:35 +0530

    feat: setup stylelinting

--- dalchini/src/routes/App.tsx
+++ dalchini/src/routes/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from '../logo.svg';
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="app">
+      <header className="app-header">
+        <img src={logo} className="app-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="app-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
