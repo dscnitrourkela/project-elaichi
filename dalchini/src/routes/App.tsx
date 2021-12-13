76456becdf74145924b470be93f6534bdba331ce
Ritesh Patil <riteshsp2000@gmail.com>
2021-12-13 17:36:16 +0530

    feat: add animations to page transitions

--- dalchini/src/routes/App.tsx
+++ dalchini/src/routes/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 // Libraries
-import { Router, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch, useLocation } from 'react-router-dom';
+import { useTransition, animated } from 'react-spring';
 
 // Components
 import { Compose, Home, MailView, Playground } from 'pages';
@@ -10,16 +11,63 @@ import { Compose, Home, MailView, Playground } from 'pages';
 import { history } from 'utils';
 
 const App: React.FC = () => {
+  const location = useLocation();
+  const transitions = useTransition(location.pathname, {
+    from: { transform: 'translateX(100vw)' },
+    enter: { transform: 'translateX(0)' },
+    leave: { transform: 'translateX(-50vw)' }
+  });
+
+  const ROUTES = [
+    {
+      path: '/',
+      component: Home
+    },
+    {
+      path: '/view/:id',
+      component: MailView
+    },
+    {
+      path: '/compose',
+      component: Compose
+    },
+    {
+      path: '/playground',
+      component: Playground
+    }
+  ];
+
+  return (
+    <>
+      <Switch>
+        {transitions((style, item) => {
+          const RouteDetails = ROUTES.filter(({ path }) =>
+            item.split('/')[1] === 'view'
+              ? item.split('/')[1] === path.split('/')[1]
+              : item === path
+          );
+
+          return (
+            <animated.div style={style}>
+              <Route
+                exact
+                path={RouteDetails[0].path}
+                component={RouteDetails[0].component}
+              />
+            </animated.div>
+          );
+        })}
+      </Switch>
+    </>
+  );
+};
+
+const MainRouter: React.FC = () => {
   return (
     <Router history={history}>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/view/:id" component={MailView} />
-        <Route exact path="/compose" component={Compose} />
-        <Route exact path="/playground" component={Playground} />
-      </Switch>
+      <App />
     </Router>
   );
 };
 
-export default App;
+export default MainRouter;
