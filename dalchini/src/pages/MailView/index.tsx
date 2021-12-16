c7d0784b2ddaf7af3789cdfceb696d5a72d5c013
Ritesh Patil <riteshsp2000@gmail.com>
2021-12-16 18:39:32 +0530

    fix: showcase attachments in the mail

--- dalchini/src/pages/MailView/index.tsx
+++ dalchini/src/pages/MailView/index.tsx
@@ -1,16 +1,36 @@
 import React, { useState, useEffect } from 'react';
 
 // Libraries
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faDownload, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useLocation } from 'react-router-dom';
 import { simpleParser, ParsedMail } from 'mailparser';
 
 // Components
-import { PageTitle, H2, P1, HighlightText, Container } from 'components';
+import {
+  PageTitle,
+  H2,
+  P1,
+  P2,
+  HighlightText,
+  Container,
+  Flexbox
+} from 'components';
 
 // Assets
 import './styles.scss';
-import { changeHistory, api } from 'utils';
+import { changeHistory, api, getQueryParam } from 'utils';
+
+// @ts-ignore
+function downloadURI(uri, name) {
+  const link = document.createElement('a');
+  link.setAttribute('download', name);
+  link.setAttribute('href', uri);
+  link.setAttribute('target', '_blank');
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
 
 const MailView: React.FC = () => {
   const [parsedMail, setParsedMail] = useState<ParsedMail | null>(null);
@@ -68,9 +88,54 @@ const MailView: React.FC = () => {
 
               <P1
                 dangerouslySetInnerHTML={{
-                  __html: parsedMail.textAsHtml || ''
+                  __html:
+                    (parsedMail?.from?.text.split('@')[1] === 'nitrkl.ac.in'
+                      ? parsedMail.textAsHtml
+                      : parsedMail.html) || ''
                 }}
               />
+
+              {parsedMail.attachments.length > 0 && (
+                <Flexbox
+                  flexColumn
+                  justifyStart
+                  alignStart
+                  className="attachments-container"
+                >
+                  {parsedMail.attachments.map(
+                    // @ts-ignore
+                    ({ checksum, filename, partId }) => (
+                      <Flexbox
+                        justifyStart
+                        alignCenter
+                        key={checksum}
+                        className="attachment"
+                        onClick={
+                          () =>
+                            downloadURI(
+                              // eslint-disable-next-line max-len
+                              `https://mail.nitrkl.ac.in/home/~/?auth=qp&zauthtoken=${getQueryParam(
+                                'zauthtoken'
+                              )}&id=${pathname.split('/')[2]}&part=${partId}`,
+                              filename
+                            )
+                          // window.open(
+                          // eslint-disable-next-line max-len
+                          //   // `https://mail.nitrkl.ac.in/service/home/~/&auth=qp?zauthtoken=${getQueryParam(
+                          //   //   'zauthtoken'
+                          //   // )}&id=${pathname.split('/')[2]}&part=${partId}`,
+                          //   // '_blank'
+                          // )
+                        }
+                      >
+                        <FontAwesomeIcon icon={faDownload} />
+
+                        <P2>{filename}</P2>
+                      </Flexbox>
+                    )
+                  )}
+                </Flexbox>
+              )}
             </>
           )}
         </div>
