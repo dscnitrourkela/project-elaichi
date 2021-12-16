91d3019a9eaf7cc406e62cf8a224175558b98956
Ritesh Patil <riteshsp2000@gmail.com>
2021-12-16 11:57:37 +0530

    feat: setup axios for api calls and create utility funtions - getQueryParams, apicall

--- dalchini/src/pages/Home/index.tsx
+++ dalchini/src/pages/Home/index.tsx
@@ -16,12 +16,33 @@ import {
 import './styles.scss';
 import { changeHistory } from 'utils';
 import MailLoading from 'assets/mail-loading.gif';
+import { api } from 'utils';
 
 const Home: React.FC = () => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     setTimeout(() => setLoading(false), 5000);
+    const fetchData = async () => {
+      try {
+        console.time();
+        const { data } = await api.get('/', {
+          params: {
+            // url: 'inbox.json'
+            // url: '/',
+            id: 13440
+          },
+          headers: {
+            Accept: '*/*'
+          }
+        });
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
   }, []);
 
   return (
@@ -51,7 +72,14 @@ const Home: React.FC = () => {
           </div>
 
           <FloatingActionButton
-            onClick={() => changeHistory('push', 'compose')}
+            // onClick={() => changeHistory('push', 'compose')}
+            onClick={() =>
+              window.open(
+                // eslint-disable-next-line max-len
+                'https://mail.nitrkl.ac.in/service/home/~/?auth=qp&zauthtoken=0_f87c11d0d73d10073bbcc0b44c56288e90e2f5f1_69643d33363a39393135363736382d326566662d343162352d613533302d3834316664353035306337323b6578703d31333a313633393735363334313434353b747970653d363a7a696d6272613b7469643d393a3536363334343533363b76657273696f6e3d31333a382e362e305f47415f313135333b&view=text&id=13440&part=2',
+                '_blank'
+              )
+            }
           />
         </>
       )}
