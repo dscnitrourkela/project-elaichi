7c3c68d1c20a257ae7e3a6c29103e93c24291a0d
Shadan Hussain <122110545+shadan1234@users.noreply.github.com>
2023-10-26 17:15:20 +0530

    Fixed Guest and Workshop (#212)

--- elaichi/lib/presentation/core/utils/event_type_map.dart
+++ elaichi/lib/presentation/core/utils/event_type_map.dart
@@ -2,8 +2,8 @@ Map<String, String> get eventTypeMapping => {
       'ALL': 'All Events',
       'PRO': 'Pro Shows',
       'TECHNICAL': 'Technical Events',
-      'GUEST-LECTURES ': 'Guest Lectures',
+      'GUEST-LECTURES': 'Guest Lectures',
       'EXHIBITIONS': 'Exhibitions',
       'FUN': 'Fun Events',
-      'WORKSHOPS': 'Workshops',
+      'WORKSHOP': 'Workshops',
     };
