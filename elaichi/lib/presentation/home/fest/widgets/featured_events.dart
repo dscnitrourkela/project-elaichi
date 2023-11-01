c3396ee119d11baa5e8549a8aa94420ec028a1e9
Mixel2004 <84668201+Mixel2004@users.noreply.github.com>
2023-11-01 13:43:58 +0530

    update: Added boarder to hacknitr event

--- elaichi/lib/presentation/home/fest/widgets/featured_events.dart
+++ elaichi/lib/presentation/home/fest/widgets/featured_events.dart
@@ -32,86 +32,99 @@ class FeaturedEventCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 280,
+      height: 250,
       child: PhysicalModel(
         elevation: 3,
         color: Colors.black,
-        shadowColor: Colors.grey,
-        child: Column(
-          children: [
-            Image.asset(
-              Strings.hackNITR,
-              height: 100,
-              fit: BoxFit.fill,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.black,
+            border: Border.all(
+              color: AppColors.grey16.withOpacity(0.6),
+              width: 2,
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(height: 12),
-                  const SizedBox(height: 8),
-                  SizedBox(
-                    height: 32,
-                    child: Text(
-                      'Explore further into your innovation journey with HackNITR 5.0 and manifest your skills.',
-                      style: interTextTheme.bodyMedium!.copyWith(
-                        fontWeight: FontWeight.w500,
-                        color: AppColors.grey,
-                        fontSize: 12,
-                        height: 1.33,
+          ),
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            children: [
+              Image.asset(
+                Strings.hackNITR,
+                height: 75,
+                fit: BoxFit.fill,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 12),
+                    const SizedBox(height: 8),
+                    SizedBox(
+                      height: 32,
+                      child: Text(
+                        'Explore further into your innovation journey with HackNITR 5.0 and manifest your skills.',
+                        style: interTextTheme.bodyMedium!.copyWith(
+                          fontWeight: FontWeight.w500,
+                          color: AppColors.grey,
+                          fontSize: 12,
+                          height: 1.33,
+                        ),
                       ),
                     ),
-                  ),
-                  const SizedBox(height: 16),
-                  const Divider(
-                    thickness: 1,
-                    height: 1,
-                    color: AppColors.grey16,
-                  ),
-                  const SizedBox(height: 12),
-                  SizedBox(
-                    height: 50,
-                    width: 326,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const DurationDates(
-                          text: 'Coming Soon.',
-                          color: AppColors.grey,
-                          fontSize: 14,
-                        ),
-                        SizedBox(
-                          height: 50,
-                          width: 110,
-                          child: TextButton(
-                            onPressed: () {
-                              launchUrlString('https://hacknitr.com/');
-                            },
-                            style: TextButton.styleFrom(
-                              backgroundColor: AppColors.lightBlue,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(25),
+                    const SizedBox(height: 16),
+                    const Divider(
+                      thickness: 1,
+                      height: 1,
+                      color: AppColors.grey16,
+                    ),
+                    const SizedBox(height: 12),
+                    SizedBox(
+                      height: 50,
+                      width: 326,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const DurationDates(
+                            text: 'Coming Soon.',
+                            color: AppColors.grey,
+                            fontSize: 14,
+                          ),
+                          SizedBox(
+                            height: 50,
+                            width: 110,
+                            child: TextButton(
+                              onPressed: () {
+                                launchUrlString(
+                                  'https://hacknitr.com/',
+                                  mode: LaunchMode.externalApplication,
+                                );
+                              },
+                              style: TextButton.styleFrom(
+                                backgroundColor: AppColors.lightBlue,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(25),
+                                ),
                               ),
-                            ),
-                            child: Text(
-                              'Visit Website',
-                              style: interTextTheme.bodyMedium!.copyWith(
-                                fontWeight: FontWeight.w500,
-                                height: 1.28,
-                                color: Colors.white,
-                                letterSpacing: -0.41,
+                              child: Text(
+                                'Visit Website',
+                                style: interTextTheme.bodyMedium!.copyWith(
+                                  fontWeight: FontWeight.w500,
+                                  height: 1.28,
+                                  color: Colors.white,
+                                  letterSpacing: -0.41,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
